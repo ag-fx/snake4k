cb3e66ac0a4ebab316142f0d03967c46aff1b664
christophpickl <christoph.pickl@gmail.com>
2018-10-17 07:50:07 +0200

    make it single distributable

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,22 @@ dependencies {
 repositories {
     jcenter()
 }
+
+application {
+    mainClassName = "com.github.christophpickl.snake4k.Snake4k"
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}-fat"
+    manifest {
+        attributes["Main-Class"] = application.mainClassName
+    }
+    from(configurations.runtime.map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks["jar"] as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
+}
