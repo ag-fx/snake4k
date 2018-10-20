fd717af5bf7c466ccd5a2933f1987737363c440d
christophpickl <christoph.pickl@gmail.com>
2018-10-20 13:08:56 +0200

    dont react on keyboard while paused

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Good old snake game wrapped in a Java desktop application.
 # Ideas
 
 * Settings option
-    * Map size (have to resize the window)
     * Option: Can go through walls
     * Option: Small delay when hit something
 * Improved highscore: based on settings-permutation
--- src/main/kotlin/com/github/christophpickl/snake4k/view/KeyboardWatcher.kt
+++ src/main/kotlin/com/github/christophpickl/snake4k/view/KeyboardWatcher.kt
@@ -1,18 +1,26 @@
 package com.github.christophpickl.snake4k.view
 
 import com.github.christophpickl.snake4k.board.Direction
+import com.github.christophpickl.snake4k.model.GameState
+import com.github.christophpickl.snake4k.model.State
 import javafx.event.EventHandler
 import javafx.scene.input.KeyEvent
 import mu.KotlinLogging
 import java.util.LinkedList
+import javax.inject.Inject
 
-class KeyboardWatcher : EventHandler<KeyEvent> {
+class KeyboardWatcher @Inject constructor(
+    private val state: State
+) : EventHandler<KeyEvent> {
 
     private val log = KotlinLogging.logger {}
 
     val collectedDirections = LinkedList<Direction>()
 
     override fun handle(event: KeyEvent) {
+        if (state.gameState != GameState.Running) {
+            return
+        }
         Direction.byKeyCode(event.code)?.let {
             log.debug { "Pressed direction key: $it" }
             if (collectedDirections.isEmpty() || collectedDirections.last != it) {
